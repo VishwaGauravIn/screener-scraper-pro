18c5f4bbbe6c464ca8bac89c010b7787aea89f78
Vishwa Gaurav <vishwagauravin@gmail.com>
2026-01-18 16:34:47 +0530

    gitignore + cleanup

--- dist/indes.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=indes.d.ts.map
--- dist/indes.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log(999);
-//# sourceMappingURL=indes.js.map
